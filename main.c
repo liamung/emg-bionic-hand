398c1523d8b67b3f02b688739b2f79334af4cfe7
liam ung <144394107+liamung@users.noreply.github.com>
2023-11-20 23:01:06 -0500

    EMG Sensor Toggled LED
    Implemented EMG Sensor patches to interface with STM microprocessor. EMG sensors produce analogue output, in turn affecting intensity of LED.

--- main.c
+++ main.c
@@ -101,39 +101,32 @@ int main(void)
   MX_USART2_UART_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start(&htim1);
-  HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);  // pull the TRIG pin low
+  //HAL_TIM_Base_Start(&htim1);
+  //HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);  // pull the TRIG pin low
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_SET);  // pull the TRIG pin HIGH
-	      __HAL_TIM_SET_COUNTER(&htim1, 0);
-	      while (__HAL_TIM_GET_COUNTER (&htim1) < 10);  // wait for 10 us
-	      HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);  // pull the TRIG pin low
+	  // IF Button Is Pressed
+	        if(HAL_GPIO_ReadPin (GPIOA, GPIO_PIN_8))
+	        {
+	            // Set The LED ON!
+	            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+	        }
+	        else
+	        {
+	            // Else .. Turn LED OFF!
+	            HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+	        }
 
-	      pMillis = HAL_GetTick(); // used this to avoid infinite while loop  (for timeout)
-	      // wait for the echo pin to go high
-	      while (!(HAL_GPIO_ReadPin (ECHO_PORT, ECHO_PIN)) && pMillis + 10 >  HAL_GetTick());
-	      Value1 = __HAL_TIM_GET_COUNTER (&htim1);
 
-	      pMillis = HAL_GetTick(); // used this to avoid infinite while loop (for timeout)
-	      // wait for the echo pin to go low
-	      while ((HAL_GPIO_ReadPin (ECHO_PORT, ECHO_PIN)) && pMillis + 50 > HAL_GetTick());
-	      Value2 = __HAL_TIM_GET_COUNTER (&htim1);
-
-	      Distance = (Value2-Value1)* 0.034/2;
-	      HAL_Delay(50);
-
-	      if(Distance > 50) {
-	  	  	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 1);
-	  		HAL_Delay(10);
-	  		HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 0);
+	  	  	//HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 1);
+	  		//HAL_Delay(10);
+	  		//HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, 0);
 	  		//HAL_Delay(100);
 
-	      }
 
     /* USER CODE END WHILE */
 
